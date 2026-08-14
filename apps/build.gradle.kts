c0aa1030ed9d852313b89257165f2fa35de2f4b4
Angesh <angeshbhardwaj@outlook.com>
2026-08-14 13:34:22 +0000

    Scaffold apps/mobile/android Gradle module and containerized build tooling

--- apps/build.gradle.kts
+++ apps/build.gradle.kts
@@ -2,8 +2,17 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.plugin.spring) apply false
+    alias(libs.plugins.kotlin.plugin.compose) apply false
+    alias(libs.plugins.kotlin.plugin.serialization) apply false
+    alias(libs.plugins.ksp) apply false
     alias(libs.plugins.spring.boot) apply false
     alias(libs.plugins.spring.dependency.management) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.android.kotlin.multiplatform.library) apply false
+    alias(libs.plugins.hilt.android) apply false
+    alias(libs.plugins.detekt) apply false
+    alias(libs.plugins.android.junit5) apply false
 }
 
 allprojects {
@@ -11,6 +20,7 @@ allprojects {
     version = "0.1.0-SNAPSHOT"
 
     repositories {
+        google()
         mavenCentral()
     }
 }
